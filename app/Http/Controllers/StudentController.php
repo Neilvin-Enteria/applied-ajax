73a2d726340be64134af564cfa1cd4dad10603cf
Neilvin Enteria <75875567+Neilvin-Enteria@users.noreply.github.com>
2022-02-08 00:31:44 +0800

    Insertion of Controller Functions

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+        
+    }
+
+    public function getAllStudents(){
+    
+    }
+
+    public function registerStudent(){
+
+    }
+
+    public function selectStudent(){
+
+    }
+
+    public function updateStudentRecord(){
+
+    }
+
+    public function archiveStudentRecord(){
+        
+    }
+
 }
--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function getAllTeacher(){
+
+    }
+
+    public function registerTeacher(){
+
+    }
+
+    public function selectTeacher(){
+
+    }
+
+    public function updateTeacherRecord(){
+
+    }
+
+    public function viewStudents(){
+
+    }
+
+    public function archiveTeacherRecords(){
+        
+    }
+
 }
